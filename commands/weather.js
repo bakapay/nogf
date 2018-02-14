68aee02836b4b4ddc29bb0402fce0435cecbb2dd
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-02-14 17:19:23 +0200

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -1,42 +1,7 @@
-const weather = require('weather-js');
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 let xemoji = client.guilds.get("411904047797239820").emojis.find("name", "chromaticX")
 let vemoji = client.guilds.get("411904047797239820").emojis.find("name", "chromaticV")
-const k = new Discord.RichEmbed()
-.setAuthor("Command: ch!weather", client.user.avatarURL)
-.setDescription("**Description:** Shows the weather in a certain location\n**Usage:** ch!wather [location]\n**Example:** ch!weather New York")
-.setFooter("Requested by " + message.author.username)
-.setColor(0x070707)
-
-if(!args.join(" ")) return message.channel.send(k)
-
-    weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
-	if (err) message.channel.send(err);
-	
-	if (result.length === 0) {
-    message.channel.send(k)
-    return;
-    }
-	
-    var current = result[0].current;
-    var location = result[0].location;
-
-	const wht = new Discord.RichEmbed()
-.setAuthor(`Weather for ${current.observationpoint}`, current.imageUrl)
-.setThumbnail(current.imageUrl)
-.setColor(0x070707)
-.addField('Timezone',`UTC${location.timezone}`, true)
-.addField('Degree Type',location.degreetype, true)
-.addField('Temperature',`${current.temperature} Degrees`, true)
-.addField('Feels Like', `${current.feelslike} Degrees`, true)
-.addField('Winds',current.winddisplay, true)
-.addField('Humidity', `${current.humidity}%`, true) 
-.setFooter("Sky - " + current.skytext)
-
-
-message.channel.send(wht)
-	});
 };
 
 exports.conf = {
@@ -47,7 +12,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'weather',
+  name: 'kogj0ju4j40jg04gj40gj04g4ggrg',
   description: 'COX.',
   usage: 'search <words>'
 };
